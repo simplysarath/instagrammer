12bdfcae11111c4030e327d1659ff2e89c95664f
Sarath Singamsetty <simplysarath@gmail.com>
2026-04-05 23:55:22 -0700

    key name updated

--- appwrite_functions/remove-background/src/main.js
+++ appwrite_functions/remove-background/src/main.js
@@ -16,9 +16,9 @@ module.exports = async ({ req, res, log, error }) => {
     return res.json({ error: 'file_id is required' }, 400);
   }
 
-  const endpoint = process.env.APPWRITE_ENDPOINT ?? process.env.APPWRITE_FUNCTION_API_ENDPOINT;
-  const projectId = process.env.APPWRITE_PROJECT_ID ?? process.env.APPWRITE_FUNCTION_PROJECT_ID;
-  const apiKey = process.env.APPWRITE_API_KEY;
+  const endpoint = process.env.APPWRITE_FUNCTION_API_ENDPOINT;
+  const projectId = process.env.APPWRITE_FUNCTION_PROJECT_ID;
+  const apiKey = process.env.SITHARA_API_KEY;
 
   log(`endpoint=${endpoint} projectId=${projectId} hasApiKey=${!!apiKey} hasRemoveBgKey=${!!process.env.REMOVEBG_API_KEY}`);
 
